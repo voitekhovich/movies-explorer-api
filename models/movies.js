d45ae9a3fe5aefa421d0ee8e8bf437b8d8293c62
voitekhovich <a.voitekhovich@gmail.com>
2022-11-28 23:27:52 +0300

    feat: Создать схемы и модели ресурсов API

---
+++ models/movies.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const { regex } = require('../utils/constans');
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    validate: (value) => regex.test(value),
+  },
+  trailerLink: {
+    type: String,
+    validate: (value) => regex.test(value),
+  },
+  thumbnail: {
+    type: String,
+    validate: (value) => regex.test(value),
+  },
+  owner: {
+    type: mongoose.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  movieId: {
+    type: String,
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+}, { versionKey: false });
+
+module.exports = mongoose.model('movie', movieSchema);
