05339dc780c3cf305e32753c33afb34b5d1edaca
voitekhovich <a.voitekhovich@gmail.com>
2022-11-30 00:29:48 +0300

    feat: Добавить логирование, cors, ddos защиту

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
 const { errors } = require('celebrate');
+
 const errs = require('./middlewares/errs');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const { cors } = require('./middlewares/cors');
+const { limiter } = require('./utils/rateLimiter');
 
 const {
   PORT = 3000,
@@ -12,11 +20,33 @@ const app = express();
 
 mongoose.connect(MONGO_DB_URL);
 
+app.use(cors);
+
+app.use(requestLogger);
+
+app.use(limiter);
+app.use(helmet());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(require('./routes'));
+
+app.use(errorLogger);
 
 app.use(errors());
 app.use(errs);
 
+const dateOptions = {
+  year: 'numeric',
+  month: 'numeric',
+  day: 'numeric',
+  timezone: 'Europe/Moscow',
+  hour: 'numeric',
+  minute: 'numeric',
+  second: 'numeric',
+};
+
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+  console.log(`${(new Date().toLocaleString('ru', dateOptions))}: App listening on port ${PORT}`);
 });
---
+++ middlewares/cors.js
@@ -0,0 +1,23 @@
+const allowedCors = [
+  // 'https://mesto.voitekhovich.nomoredomains.icu',
+  // 'http://mesto.voitekhovich.nomoredomains.icu',
+  'http://localhost:3001',
+];
+
+module.exports.cors = (req, res, next) => {
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', 'true');
+  }
+
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  return next();
+};
